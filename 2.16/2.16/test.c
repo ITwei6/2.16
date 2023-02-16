1ac5116b7af44d34b133f273e0df14997398ce89
2268129437@qq.com <2268129437@qq.com>
2023-02-16 22:23:09 +0800

    日常练习

---
+++ 2.16/2.16/test.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+
+	//char* p = "xiao tao lai lo";
+	////指针p里面存放的就是字符串的首字符地址。
+	//printf(p);
+	printf("xiao tao lai lo\n");
+	char ch[100] = "xiao tao lai lo";
+	//ch是数组名，数组名是首元素的地址，也就是首字符的地址。
+	printf(ch);
+	
+
+}
